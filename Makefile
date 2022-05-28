c3bdd6b9755d63edb4e18bc8f52f1e81f64ee8d2
danilbushkov <danil1357997@gmail.com>
2022-05-28 22:12:34 +0300

    added qtable

--- Makefile
+++ Makefile
@@ -9,7 +9,8 @@ all: app
 app: bin 	
 	g++ ./src/main.cpp -o bin/main
 
-
+check:
+	valgrind --leak-check=yes ./bin/main
 
 run: ./bin/main
 	./bin/main
--- src/main.cpp
+++ src/main.cpp
@@ -13,11 +13,11 @@ int main(){
 
     int i = 0;
     int j = 0;
-    ifstream f("./field.txt");
-    if(f.is_open()){
-        while(f>>cell){
+    ifstream in("./field.txt");
+    if(in.is_open()){
+        while(in>>cell){
             if(cell==0){
-                field[i]=0;
+                field[i]=j++;
             }else{
                 field[i]=-1;
             }
@@ -27,9 +27,61 @@ int main(){
         }
     }
 
-    f.close();
+    in.close();
+
+    float **stateAction = new float*[j];
+    for(i = 0; i<j; ++i){
+        stateAction[i] = new float[4];
+    }
+
+    in.open("./qtable.txt");
+
+    if(in.is_open()){
+        float k;
+
+        i = 0;
+        int m = 0;
+        while(in >> k){
+            if(i<j){
+                stateAction[i][m]=k;
+            }
+            if(m == 3){
+                m = 0;
+                ++i;
+            }else{
+                ++m;
+            }
+            
+        }
+    }
+
+    in.close();
 
 
+
+
+
+    ofstream out("./qtable.txt");
+    
+    if(out.is_open()){
+        //out << j << endl;
+        for(i = 0; i<j; i++){
+            
+            //out << i << ' ';
+            for(int k = 0; k<4; ++k){
+                //stateAction[i][k] = 0.0;
+                out << stateAction[i][k] << ' ';
+            }
+            out << endl;
+        }
+        
+    }
+    out.close();
+
+    for(int i = 0; i<j; i++){
+        delete [] stateAction[i];
+    }
+    delete [] stateAction;
 
 
     for(int i = 0; i<100; ++i){
@@ -40,7 +92,7 @@ int main(){
         }
 
         if((i+1)%10==0){
-            cout<<endl;
+            cout<<'|'<<endl;
         }
     }
 
